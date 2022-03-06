9e6acd7291d826b6b65d135582fe2df535c9e37b
Rosane Johann Braun <91096326+RosaneJB@users.noreply.github.com>
2022-03-05 21:00:20 -0400

    Removido texto

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -8,12 +8,11 @@ export const Home = () => {
     const changeDetails = (details: MovieDetailsType) => dispatch(setDetails(details))
 
     const handleTitle = () => {
-        changeDetails({...movies.details, title: 'Novo Título', original_language: 'PT-BR'})
+        changeDetails({ ...movies.details, title: 'Novo Título', original_language: 'PT-BR' })
     }
 
     return (
         <>
-        <h1>{movies.details.title} - {movies.details.original_language} - {movies.upcoming.page}- {movies.upcoming.total_pages}</h1>
             <button className="btn btn-success" onClick={handleTitle}>
                 Alterar Título
             </button>
